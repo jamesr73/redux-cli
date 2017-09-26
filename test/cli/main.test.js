6b9abf7e362ac0847e58aeb2795e06b92c01fad2
James Robertson <jamesr73.github@icloud.com>
2017-09-26 18:28:44 +0100

    Remove commander cli implementation.

--- test/cli/main.test.js
+++
@@ -1,9 +0,0 @@
-import parser from 'cli/main';
-
-describe('(CLI) Main', () => {
-  describe('bare command', () => {
-    test('outputs help', () => {
-      expect(true).toEqual(true);
-    });
-  });
-});
